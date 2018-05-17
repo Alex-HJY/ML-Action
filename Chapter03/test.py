5eed9843e5452efe3b3c0153c511dd742ab49a0f
Alex-H <AlexanderHJY@icloud.com>
2018-05-17 17:55:57 +0800

    5-17 chap03

---
+++ Chapter03/test.py
@@ -0,0 +1,118 @@
+import operator
+from math import log
+
+import treePlotter
+
+
+def create_dataset():
+    dataset = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataset, labels
+
+
+def calc_shannonent(dataset):
+    nam_entries = len(dataset)
+    label_counts = {}
+    for vec in dataset:
+        current_label = vec[-1]
+        label_counts[current_label] = label_counts.get(current_label, 0) + 1
+    shannonent = 0
+    for k, v in label_counts.items():
+        prob = v / nam_entries
+        shannonent -= prob * log(prob, 2)
+    return shannonent
+
+
+def split_dataset(dataset, index, value):
+    ret_dataset = []
+    for feat_vec in dataset:
+        if feat_vec[index] == value:
+            reduced_feat_vec = feat_vec[:index] + feat_vec[index + 1:]
+            ret_dataset.append(reduced_feat_vec)
+    return ret_dataset
+
+
+def choose_best_feature_to_split(dataset):
+    num_features = len(dataset[0]) - 1
+    base_entropy = calc_shannonent(dataset)
+    best_infogain, best_feature = 0, -1
+    for i in range(num_features):
+        feat_list = [vec[i] for vec in dataset]
+        feat_set = set(feat_list)
+        temp_entropy = 0
+        for value in feat_set:
+            sub_dataset = split_dataset(dataset, i, value)
+            prob = len(sub_dataset) / len(dataset)
+            temp_entropy += prob * calc_shannonent(sub_dataset)
+        temp_infogain = base_entropy - temp_entropy
+        print('infoGain=', temp_infogain, 'bestFeature=', i, base_entropy, temp_entropy)
+        if temp_infogain > best_infogain:
+            best_infogain = temp_infogain
+            best_feature = i
+    return best_feature
+
+
+def majority_cnt(class_list):
+    classCount = {}
+    for vote in class_list:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def create_tree(dataset, labels):
+    class_list = [vec[-1] for vec in dataset]
+    if class_list.count(class_list[0]) == class_list.__len__():
+        return class_list[0]
+
+    if len(dataset[0]) == 1:
+        return majority_cnt(class_list)
+
+    best_feat = choose_best_feature_to_split(dataset)
+    best_feat_label = labels[best_feat]
+    my_tree = {best_feat_label: {}}
+    feat_values = [vec[best_feat] for vec in dataset]
+    unique_values = set(feat_values)
+    sublabels = labels[:]
+    del (sublabels[best_feat])
+    for value in unique_values:
+        my_tree[best_feat_label][value] = create_tree(split_dataset(dataset, best_feat, value), sublabels)
+    return my_tree
+
+
+def classify(input_tree, feat_labels, test_vec):
+    first_str = list(input_tree.keys())[0]
+    second_dict = input_tree[first_str]
+    feat_index = feat_labels.index(first_str)
+    key = test_vec[feat_index]
+    valueOfFeat = second_dict[key]
+    print('+++', first_str, 'xxx', second_dict, '---', key, '>>>', valueOfFeat)
+    if isinstance(valueOfFeat, dict):
+        classlabel = (classify(valueOfFeat, feat_labels, test_vec))
+    else:
+        classlabel = valueOfFeat
+    return classlabel
+
+
+def store_tree(inputtree, filename):
+    import pickle
+    with open(filename, 'wb') as f:
+        pickle.dump(inputtree, f)
+
+
+def grab_tree(filename):
+    import pickle
+    with open(filename, 'rb') as f:
+        return pickle.loads(f)
+
+
+with open('.\Chapter03\lenses.txt', 'r') as fr:
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+lensesTree = create_tree(lenses, lensesLabels)
+treePlotter.createPlot(lensesTree)
--- chapter02/knn.py
+++ chapter02/knn.py
@@ -1,72 +1,87 @@
-import operator
+'''
+Created on Sep 16, 2010
+kNN: k Nearest Neighbors
+
+Input:      inX: vector to compare to existing dataset (1xN)
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison (should be an odd number)
+            
+Output:     the most popular class label
+
+@author: pbharrin
+'''
+
 from os import listdir
 
-import numpy as np
+from numpy import *
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+        sortedClassCount = sorted(iter(classCount.items()), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
 def createDataSet():
-    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
 
-def classify0(inX, dataset, labels, k):
-    datasetsize = dataset.shape[0]
-    diffmat = np.tile(inX, (datasetsize, 1)) - dataset
-    distances = (diffmat ** 2).sum(axis=1) ** 0.5
-    sorted_distindicies = distances.argsort()
-    classcount = {}
-    for i in range(k):
-        voteilabel = labels[sorted_distindicies[i]]
-        classcount[voteilabel] = classcount.get(voteilabel, 0) + 1
-    sortedclasscount = sorted(classcount.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedclasscount[0][0]
-
-
 def file2matrix(filename):
-    with open(filename) as f:
-        numberoflines = len(f.readlines())
-    returnmat = np.zeros((numberoflines, 3))
-    classlabelvector = []
-    with open(filename) as f:
-        filelines = f.readlines()
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())  # get the number of lines in the file
+    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+    classLabelVector = []  # prepare labels return
+    fr = open(filename)
     index = 0
-    for line in filelines:
+    for line in fr.readlines():
         line = line.strip()
-        listfromline = line.split('\t')
-        returnmat[index, :] = listfromline[0:3]
-        classlabelvector.append(int(listfromline[-1]))
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
         index += 1
-    return returnmat, classlabelvector
+    return returnMat, classLabelVector
 
 
-def autonorm(dataset):
-    minvals = dataset.min(0)
-    maxvals = dataset.max(0)
-    ranges = maxvals - minvals
-    normdata = np.zeros(np.shape(dataset))
-    normdata = dataset - np.tile(minvals, (dataset.shape[0], 1))
-    normdata /= np.tile(ranges, (dataset.shape[0], 1))
-    return normdata, ranges, minvals
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide
+    return normDataSet, ranges, minVals
 
 
-def datingclasstest():
-    ratio = 0.5
-    dataingdatamat, datinglables = file2matrix('datingTestSet2.txt')
-    normmat, ranges, minval = autonorm(dataingdatamat)
-    m = normmat.shape[0]
-    numoftestvecs = int(ratio * normmat.shape[0])
-    errorcount = 0
-    for i in range(numoftestvecs):
-        classifierresult = classify0(normmat[i, :], normmat[numoftestvecs:m, :], datinglables[numoftestvecs:m], 3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierresult, datinglables[i]))
-        if (classifierresult != datinglables[i]): errorcount += 1
-    print("the total error rate is: %f" % (errorcount / float(numoftestvecs)))
-    print(errorcount)
+def datingClassTest():
+    hoRatio = 0.50  # hold out 10%
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')  # load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+    print(errorCount)
 
 
 def img2vector(filename):
-    returnVect = np.zeros((1, 1024))
+    returnVect = zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
@@ -79,7 +94,7 @@ def handwritingClassTest():
     hwLabels = []
     trainingFileList = listdir('trainingDigits')  # load the training set
     m = len(trainingFileList)
-    trainingMat = np.zeros((m, 1024))
+    trainingMat = zeros((m, 1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]  # take off .txt
