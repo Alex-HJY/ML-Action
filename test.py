440b1dfb0989e129b1acb5e7c84d1732229df052
Alex-H <AlexanderHJY@icloud.com>
2018-05-17 09:25:06 +0800

    5-17

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from jqdatasdk import *
-
-auth('15650797972', '123456789')
+def createDataSet():
+	dataSet = [[1, 1, 'yes'],
+	           [1, 1, 'yes'],
+	           [1, 0, 'no'],
+	           [0, 1, 'no'],
+	           [0, 1, 'no']]
+	labels = ['no surfacing', 'flippers']
+	return dataSet, labels
